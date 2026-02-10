744250ea3bed778a59b7818a9947cff9a468ac40
arunsketh <arunsankarmoto@gmail.com>
2026-02-10 12:58:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ if uploaded_files:
                         if fmt == 'JPG': fmt = 'JPEG'
                         cropped_img.save(img_byte_arr, format=fmt)
                         
-                        zf.writestr(f"cropped_{file.name}", img_byte_arr.getvalue())
+                        zf.writestr(f"{file.name}_Cropped", img_byte_arr.getvalue())
                     except Exception as e:
                         print(f"Error: {e}")
                     
